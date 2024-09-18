bcb7710f3b8d2294f8ab550765c2e1e4c678c6d4
Rey Mooy <58579641+reymooy27@users.noreply.github.com>
2024-09-18 10:42:08 +0800

    build script for windows

--- lua/plugins/avante-ai.lua
+++ lua/plugins/avante-ai.lua
@@ -21,8 +21,8 @@ return {
         -- add any opts here
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    -- build = "make",
+    build = "pwsh -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false", -- for windows
     dependencies = {
         "stevearc/dressing.nvim",
         "nvim-lua/plenary.nvim",
