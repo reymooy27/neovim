535df1dd6722952821647fd779f2a28a72df5e04
Rey Mooy <58579641+reymooy27@users.noreply.github.com>
2024-12-24 04:10:04 +0800

    upfate

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -3,7 +3,7 @@ return {
     config = function()
         require("supermaven-nvim").setup({
             keymaps = {
-                accept_suggestion = "<C-l>",
+                accept_suggestion = "<Tab>",
                 clear_suggestion = "<C-]>",
             },
         })
