7f7084dcdb5d68e46a52faded26803a72d725cf6
Rey Mooy <58579641+reymooy27@users.noreply.github.com>
2025-01-28 00:50:21 +0800

    update

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
     -- -- disable trouble
     -- { "dstein64/vim-startuptime", enabled = false },
-    -- { "folke/noice.nvim", enabled = false },
+    { "folke/noice.nvim", enabled = false },
     -- { "folke/todo-comments.nvim", enabled = false },
     -- { "folke/trouble.nvim", enabled = false },
     -- { "folke/persistence.nvim", enabled = false },
